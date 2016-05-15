4435e9ec9ec42c78ff1fb1584b3bf3d05b4fe793
naweskil <naweskil@gmail.com>
2016-05-14 23:10:10 -0300

    Função de contar número de soldados criada

--- josephusProblem/src/josephusProblem.c
+++ josephusProblem/src/josephusProblem.c
@@ -21,38 +21,41 @@ struct soldado {
 	SLista* prox;
 };
 
-//Auxiliares
+// Auxiliares
 SLista* cria_soldado(char *nome, int numero);
 
 SLista* inicializa();
 int verificaCircVazio(SLista* lista);
 SLista* insereSoldadoNoCirc(SLista* lista, char *nome, int num);
 void imprimeSoldadosCirc(SLista* lista);
+int verificaQteSoldados(SLista* lista);
 
 int main(void) {
 
-	//Proposito de testes
+	// Proposito de testes
 	char nomes[5][20]  = { "Joao", "Arthur", "Davi", "Joaquim", "Eduardo" };
 
 	int i;
 	SLista *listax = inicializa();
 
-	//Insere os 5 soldados na lista
+	// Insere os 5 soldados na lista
 	for (i = 0; i < 5; i++)  {
 		listax = insereSoldadoNoCirc(listax, nomes[i], (i+1));
 	}
 
-	//Imprime
+	// Imprime
 	imprimeSoldadosCirc(listax);
 
+	// Quantidade de Soldados
+	printf("\nQuantidade de Soldados: %d", verificaQteSoldados(listax));
 
 	return EXIT_SUCCESS;
 }
 
-
+// Aloca na memória um soldado apontando pra ele mesmo
 SLista* cria_soldado(char *nome, int numero) {
 
-	SLista *soldado = (SLista *)malloc(sizeof(SLista)); //usando calloc pq ele aloca e inicializa
+	SLista* soldado = (SLista*)malloc(sizeof(SLista));
 	soldado->num = numero;
 	strcpy(soldado->nome, nome);
 	soldado->prox = soldado;
@@ -62,41 +65,38 @@ SLista* cria_soldado(char *nome, int numero) {
 
 SLista* insereSoldadoNoCirc(SLista* lista, char *nome, int num) {
 
-	//aloca na memória um soldado apontando pra ele mesmo
+	// Alocação do soldado na memória
 	SLista *soldado = cria_soldado(nome, num);
 
-	//se lista existit (se não estiver vazia):
-	//percorre até encontrar o ultimo elemento
-	//liga o ´prox´ do novo soldado na lista
-	//liga o ´prox´ do ultimo soldado no novo soldado
-	if (lista)
-	{
-		SLista *aux = lista;
+	/* Se lista existir (se não estiver vazia),
+	 * percorre até encontrar o último elemento
+	 * liga o 'prox' do novo soldado na lista,
+	 * liga o 'prox' do último soldado no novo soldado.
+	 */
+	if (lista) {
+		SLista* aux = lista;
 
 		while (aux->prox != lista)
 			aux = aux->prox;
 
-		aux->prox = soldado;
-		soldado->prox = lista;
+		aux->prox = soldado; // Liga o último elemento da lista no novo registro
+		soldado->prox = lista; // Fecha a lista circular, ligando o último registro na lista
 	}
 
 	return soldado;
 }
 
-
 void imprimeSoldadosCirc(SLista* lista) {
 
 	SLista* aux = lista;
 
-	if (!verificaCircVazio(lista))
-	{
+	if ( !verificaCircVazio(lista) ) {
 		do {
-				printf("Soldado: [%d] %s [%p]->[%p]\n", aux->num, aux->nome, aux, aux->prox);
-				aux = aux->prox;
+			printf("Soldado: [%d] %s [%p]->[%p]\n", aux->num, aux->nome, aux, aux->prox);
+			aux = aux->prox;
 
-			} while (aux != lista);
+		} while (aux != lista);
 	}
-
 }
 
 SLista* inicializa() {
@@ -106,4 +106,19 @@ SLista* inicializa() {
 
 int verificaCircVazio(SLista* lista) {
 	return !lista;
+}
+
+int verificaQteSoldados(SLista* lista) {
+	int qtd = 0;
+	SLista* aux = lista;
+
+	if ( !verificaCircVazio(lista) ) {
+		qtd++;
+		while (aux->prox != lista) {
+			qtd++;
+			aux = aux->prox;
+		}
+	}
+
+	return qtd;
 }
